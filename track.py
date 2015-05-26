d698a3f955e1d9dbbf62c975354ad7159d166f62
s-low <SAMUEL.P.LOW@GMAIL.COM>
2015-05-26 23:57:54 +0100

    Simple OpenCV script. Open local mp4, flip frames and write to .mov

---
+++ track.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy
+
+cap = cv2.VideoCapture("fk_test_1.mp4")
+
+# Define the codec and create writer from it
+fps = 30.0
+capsize = (1280,720)
+fourcc = cv2.cv.CV_FOURCC('m', 'p', '4', 'v') # note the lower case
+
+vout = cv2.VideoWriter()
+success = vout.open('output.mov',fourcc,fps,capsize,True) 
+
+while(cap.isOpened()):
+	ret, frame = cap.read()
+
+	if ret == True:
+		frame = cv2.flip(frame, 0)	
+		vout.write(frame) 
+		cv2.imshow('frame',frame)
+	
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
+	else: 
+		break
+
+cap.release()
+vout.release()
+vout = None
+cv2.destroyAllWindows()
