ba17e45aa08a444c98fbdb46d65c7d854987a137
s-low <SAMUEL.P.LOW@GMAIL.COM>
2015-06-23 12:23:09 +0100

    trajectory algorithm up to kaman initiation

--- src/kalman.py
+++ src/kalman.py
@@ -1,19 +1,21 @@
 #!/usr/local/bin/python
 
-# IN: Output.txt containing set of ball candidates for each frame
-# space delimited three column file eg:
-
+# INPUT: Output.txt containing set of ball candidates for each frame
+# format is space delimited three column file eg:
 # X    Y    F
 # 1    5    1
 # 16   3    1
 # 1    35   3
 # 20   4    4
 
-# OUT: Set of candidate trajectories
+# OUTPUT: Set of candidate trajectories
 
 import sys
 import cv2
 import numpy as np 
+
+# KALMAN PARAMETERS
+max_dist = 20
 
 with open("output.txt") as datafile:
 	data = datafile.read()
@@ -46,21 +48,35 @@ for row in data:
 # frame_array is an array of dictionaries - each containing the dataset for that
 # particular frame. 
 
-# FOR each frame F:
-for index, frame in enumerate(frame_array):
-	print index, frame
-	
-	# FOR each candidate b in F:
-	for x_i, x in enumerate(frame["x"]):
-		b_x = x 
-		b_y = frame["y"][x_i]
+# FOR each frame F0:
+for index, f0 in enumerate(frame_array):
+	print "Frame: "+ `index`
+	print f0
 
+	# always need two frames of headroom
+	if index == max_frame - 1:
+		break
 
-		# FOR each candidate b+ in F+:
-		for b_plus_i, b_plus in 
+	# next frames
+	f1 = frame_array[index + 1]
+	f2 = frame_array[index + 2]
 
+	# FOR each candidate b in F0:
+	for b in f0["x"]:
+		b_x = float(f0["x"].pop(0))
+		b_y = float(f0["y"].pop(0))
+
+		# FOR each candidate b1 in F1:
+		for b1 in f1["x"]:
+			b1_x = float(f1["x"].pop(0))
+			b1_y = float(f1["y"].pop(0))
+		
 			# IF separation between b and b+ is small:
-				
+			xdiff = abs(b_x - b1_x)
+			ydiff = abs(b_y - b1_y)
+			sep = ((xdiff**2) + (ydiff**2))**(0.5)
+			
+			# if sep < max_dist:
 				# init kalman filter
 				# predict location in F++
 
