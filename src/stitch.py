aa18c493b77fe4f41740661a1219e92cd92a76fe
s-low <SAMUEL.P.LOW@GMAIL.COM>
2015-07-03 17:05:53 +0100

    changed from tuple to list structure. caught issue where a trajectory was deleted/altered then accessed.

--- src/stitch.py
+++ src/stitch.py
@@ -17,7 +17,7 @@ TID = 0
 PID = 1
 
 for row in data:
-	tid_pid.append((row.split(' ')[0], row.split(' ')[3]))
+	tid_pid.append(list((row.split(' ')[0], row.split(' ')[3])))
 
 tid_list = []
 
@@ -27,7 +27,6 @@ for point in tid_pid:
 
 # for each trajectory
 for A in tid_list:
-	print "A:", A
 	# collect the points into a list
 	A_points = []
 	for point in tid_pid:
@@ -36,26 +35,31 @@ for A in tid_list:
 
 	# for each other trajectory
 	for B in tid_list:
-
 		# collect the trajectory into a list
 		if A!=B:
-			print "-------\nB:", B
-			raw_input()
 			B_points = []
 			for point in tid_pid:
 				if point[TID] == B:
 					B_points.append(point[PID])
+			
+			if len(B_points) > 1 and len(A_points) > 1:
+			
+				
 
-			print A_points[-2],B_points[0]
-			print A_points[-1], B_points[1]
+				# if the last two points of A are the first two points of B
+				if A_points[-1] == B_points[1]:
+					if A_points[-2] == B_points[0]:
 
-			if A_points[-1] == B_points[1]:
-				if A_points[-2] == B_points[0]:
-					print "MATCH between TIDS:", A, B
+						print "MATCH between TIDS:", A, B
+						print A_points[-2], B_points[0]
+						print A_points[-1], B_points[1]
+
+						# change tid of B to match A and and remove the duplicate points 
+						del B_points[0:1]
+						# can do this to the original data because compound stitches are allowed
+						for point in tid_pid:
+							if point[TID] == B:
+								point[TID] = A
 	
-
-	# if at least the last two points of A are the first two points of B
-		# change tid of B to match A and and remove the duplicate points 
-		# can do this to the original data because compound stitches are allowed
 
 # write the new data back to file
