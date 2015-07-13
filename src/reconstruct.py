4a32971f3446838a78a4c3d16f3ce25d51cfb364
s-low <SAMUEL.P.LOW@GMAIL.COM>
2015-07-13 22:36:54 +0100

    fundamental testing with x'Fx = 0. 8pt seemingly far better than RANSAC.

--- src/reconstruct.py
+++ src/reconstruct.py
@@ -13,26 +13,29 @@ Point = namedtuple("Point", "x y")
 
 
 def run():
-    # TEST DATA
-    # 1 = RHS
-    pts1_raw = [[423, 191],  # t_l
-                [840, 217],  # t_r
-                [422, 352],  # b_l
-                [838, 377],  # b_r
-                [325, 437],  # front_l
-                [744, 464],  # front_r
-                [288, 344],  # wide_l
-                [974, 388]]  # wide_r
+    # 1 = RHS d5000
+    pts1_raw = [[726, 267],
+                [761, 266],
+                [715, 463],
+                [749, 461],
+                [667, 432],
+                [677, 256],
+                [971, 465],
+                [448, 383],
+                [1035, 531],
+                [572, 165]]
 
-    # 2 = LHS
-    pts2_raw = [[423, 192],  # t_l
-                [841, 166],  # t_r
-                [422, 358],  # b_l
-                [839, 330],  # b_r
-                [518, 440],  # front_l
-                [934, 417],  # front_r
-                [287, 363],  # wide_l
-                [973, 320]]  # wide_r
+    # 2 = LHS G3
+    pts2_raw = [[664, 391],
+                [684, 379],
+                [660, 587],
+                [685, 571],
+                [554, 554],
+                [550, 365],
+                [924, 536],
+                [229, 554],
+                [1179, 646],
+                [342, 260]]
 
     pts1 = np.array(pts1_raw, dtype='float32')
     pts2 = np.array(pts2_raw, dtype='float32')
@@ -42,8 +45,11 @@ def run():
     K2 = np.mat(CalibArray(1091, 644, 412))  # g3
 
     # Find FUNDAMENTAL MATRIX from point correspondences
-    F, mask = cv2.findFundamentalMat(pts1, pts2, cv2.RANSAC)
+    F, mask = cv2.findFundamentalMat(pts1, pts2, cv.CV_FM_8POINT)
     print "Fundamental:\n", F
+
+    testFundamental(F, pts1, pts2)
+    sys.exit()
 
     # turn it into the ESSENTIAL MATRIX using calibration matrices K1/K2
     # where 1 corresponds to the camera with P1 = [I|0]
@@ -58,17 +64,17 @@ def run():
 
     s, u, vt = cv2.SVDecomp(E)
 
-    if np.linalg.det(np.mat(u)) < 0:
-        u *= -1.0
-    if np.linalg.det(np.mat(vt)) < 0:
-        vt *= -1.0
-
     # HZ 9.19 (may need to check all four)
-    R = np.mat(u) * np.mat(W) * np.mat(vt)
-    print "R:\n", R
-    t = u[:, 2]      # translation is third col of U
-    print "t:\n", t
+    R1 = np.mat(u) * np.mat(W) * np.mat(vt)
+    R2 = np.mat(u) * np.mat(W.T) * np.mat(vt)
+    t1 = u[:, 2]      # translation is third col of U
+    t2 = -1 * u[:, 2]
 
+    print "R:\n", R1, R2
+    print "t:\n", t1, t2
+
+    R = R2
+    t = t2
     P1 = BoringCameraArray()
     P2 = CameraArray(R, t)
 
@@ -91,6 +97,17 @@ def run():
         points3d.append(X[1])
 
     plot3D(points3d)
+
+
+def testFundamental(F, pts1, pts2):
+    # check that xFx = 0
+    pts1 = cv2.convertPointsToHomogeneous(src=pts1)
+    pts2 = cv2.convertPointsToHomogeneous(src=pts2)
+    err = 0
+    for i in range(0, len(pts1)):
+        err += np.mat(pts1[i]) * np.mat(F) * np.mat(pts2[i].T)
+
+    print "avg px error in xFx:", err / len(pts1)
 
 
 def CalibArray(focalLength, cx, cy):
@@ -160,6 +177,8 @@ def CameraArray(R, t):
 def plot3D(objectPoints):
     # Plotting of the system
     print "PLOT"
+    for point in objectPoints:
+        print point[0], point[1], point[2]
 
     all_x = [point[0] for point in objectPoints]
     all_y = [point[1] for point in objectPoints]
@@ -209,5 +228,40 @@ def LinearTriangulation(P1, u1, P2, u2):
     # print "B:\n", B
     X = cv2.solve(A, B, flags=cv2.DECOMP_SVD)
     return X
+
+
+def IterativeLinearTriangulation(P1, u1, P2, u2):
+
+    A = np.zeros((4, 3), dtype='float32')
+    B = np.zeros((4, 1), dtype='float32')
+    X = np.zeros((3, 1), dtype='float32')
+
+    A[0][0] = u1.x * P1[2][0] - P1[0][0]
+    A[0][1] = u1.x * P1[2][1] - P1[0][1]
+    A[0][2] = u1.x * P1[2][2] - P1[0][2]
+
+    A[1][0] = u1.y * P1[2][0] - P1[1][0]
+    A[1][1] = u1.y * P1[2][1] - P1[1][1]
+    A[1][2] = u1.y * P1[2][2] - P1[1][2]
+
+    A[2][0] = u2.x * P2[2][0] - P2[0][0]
+    A[2][1] = u2.x * P2[2][1] - P2[0][1]
+    A[2][2] = u2.x * P2[2][2] - P2[0][2]
+
+    A[3][0] = u2.y * P2[2][0] - P2[1][0]
+    A[3][1] = u2.y * P2[2][1] - P2[1][1]
+    A[3][2] = u2.y * P2[2][2] - P2[1][2]
+
+    # print "A:\n", A
+
+    B[0][0] = -(u1.x * P1[2][3] - P1[0][3])
+    B[0][0] = -(u1.y * P1[2][3] - P1[1][3])
+    B[0][0] = -(u2.x * P2[2][3] - P2[0][3])
+    B[3][0] = -(u2.y * P2[2][3] - P2[1][3])
+
+    # print "B:\n", B
+    X = cv2.solve(A, B, flags=cv2.DECOMP_SVD)
+    return X
+
 
 run()
