a89b7d1c46410ca041224fc8cf7ee88b005b8d84
s-low <SAMUEL.P.LOW@GMAIL.COM>
2015-07-13 18:32:36 +0100

    bulk changes - just moving methods around

--- src/reconstruct.py
+++ src/reconstruct.py
@@ -12,6 +12,87 @@ plt.style.use('ggplot')
 Point = namedtuple("Point", "x y")
 
 
+def run():
+    # TEST DATA
+    # 1 = RHS
+    pts1_raw = [[423, 191],  # t_l
+                [840, 217],  # t_r
+                [422, 352],  # b_l
+                [838, 377],  # b_r
+                [325, 437],  # front_l
+                [744, 464],  # front_r
+                [288, 344],  # wide_l
+                [974, 388]]  # wide_r
+
+    # 2 = LHS
+    pts2_raw = [[423, 192],  # t_l
+                [841, 166],  # t_r
+                [422, 358],  # b_l
+                [839, 330],  # b_r
+                [518, 440],  # front_l
+                [934, 417],  # front_r
+                [287, 363],  # wide_l
+                [973, 320]]  # wide_r
+
+    pts1 = np.array(pts1_raw, dtype='float32')
+    pts2 = np.array(pts2_raw, dtype='float32')
+
+    # Given camera CALIBRATION MATRICES
+    K1 = np.mat(CalibArray(993, 655, 371))  # d5000
+    K2 = np.mat(CalibArray(1091, 644, 412))  # g3
+
+    # Find FUNDAMENTAL MATRIX from point correspondences
+    F, mask = cv2.findFundamentalMat(pts1, pts2, cv2.RANSAC)
+    print "Fundamental:\n", F
+
+    # turn it into the ESSENTIAL MATRIX using calibration matrices K1/K2
+    # where 1 corresponds to the camera with P1 = [I|0]
+    E = K2.T * np.mat(F) * K1
+    print "Essential:\n", E
+
+    # Find CAMERA MATRICES from E
+    # where one of them has P1 = [I|0] get the second camera matrix P2 = [R|t]
+    # this relies on Hartley and Zisserman sec 9.6
+
+    W, W_inv = initSVDarrays()
+
+    s, u, vt = cv2.SVDecomp(E)
+
+    if np.linalg.det(np.mat(u)) < 0:
+        u *= -1.0
+    if np.linalg.det(np.mat(vt)) < 0:
+        vt *= -1.0
+
+    # HZ 9.19 (may need to check all four)
+    R = np.mat(u) * np.mat(W) * np.mat(vt)
+    print "R:\n", R
+    t = u[:, 2]      # translation is third col of U
+    print "t:\n", t
+
+    P1 = BoringCameraArray()
+    P2 = CameraArray(R, t)
+
+    print "P1:\n", P1
+    print "P2:\n", P2
+
+    # triangulate the points (don't use cv2.triangulatePoints)
+    points3d = []
+
+    for i in range(0, len(pts1_raw)):
+        x1 = pts1_raw[i][0]
+        y1 = pts1_raw[i][0]
+        x2 = pts2_raw[i][0]
+        y2 = pts2_raw[i][0]
+
+        u1 = Point(x1, y1)
+        u2 = Point(x2, y2)
+
+        X = LinearTriangulation(P1, u1, P2, u2)
+        points3d.append(X[1])
+
+    plot3D(points3d)
+
+
 def CalibArray(focalLength, cx, cy):
     calibArray = np.zeros((3, 3), dtype='float32')
     calibArray[0][0] = focalLength
@@ -129,79 +210,4 @@ def LinearTriangulation(P1, u1, P2, u2):
     X = cv2.solve(A, B, flags=cv2.DECOMP_SVD)
     return X
 
-# TEST DATA
-# 1 = RHS
-pts1_raw = [[423, 191],  # t_l
-            [840, 217],  # t_r
-            [422, 352],  # b_l
-            [838, 377],  # b_r
-            [325, 437],  # front_l
-            [744, 464],  # front_r
-            [288, 344],  # wide_l
-            [974, 388]]  # wide_r
-
-# 2 = LHS
-pts2_raw = [[423, 192],  # t_l
-            [841, 166],  # t_r
-            [422, 358],  # b_l
-            [839, 330],  # b_r
-            [518, 440],  # front_l
-            [934, 417],  # front_r
-            [287, 363],  # wide_l
-            [973, 320]]  # wide_r
-
-pts1 = np.array(pts1_raw, dtype='float32')
-pts2 = np.array(pts2_raw, dtype='float32')
-
-# Given camera CALIBRATION MATRICES
-K1 = np.mat(CalibArray(993, 655, 371))  # d5000
-K2 = np.mat(CalibArray(1091, 644, 412))  # g3
-
-# Find FUNDAMENTAL MATRIX from point correspondences
-F, mask = cv2.findFundamentalMat(pts1, pts2, cv2.RANSAC)
-print "Fundamental:\n", F
-
-# turn it into the ESSENTIAL MATRIX using calibration matrices K1/K2
-E = K2.T * np.mat(F) * K1  # where 1 corresponds to the camera with P1 = [I|0]
-print "Essential:\n", E
-
-# Find CAMERA MATRICES from E
-# where one of them has P1 = [I|0] get the second camera matrix P2 = [R|t]
-# this relies on Hartley and Zisserman sec 9.6
-
-W, W_inv = initSVDarrays()
-
-s, u, vt = cv2.SVDecomp(E)
-
-if np.linalg.det(np.mat(u)) < 0:
-    u *= -1.0
-if np.linalg.det(np.mat(vt)) < 0:
-    vt *= -1.0
-
-R = np.mat(u) * np.mat(W) * np.mat(vt)   # HZ 9.19 (may need to check all four)
-print "R:\n", R
-t = u[:, 2]      # translation is third col of U
-print "t:\n", t
-
-P1 = BoringCameraArray()
-P2 = CameraArray(R, t)
-
-print "P1:\n", P1
-print "P2:\n", P2
-
-# triangulate the points (don't use cv2.triangulatePoints)
-points3d = []
-
-for i in range(0, len(pts1_raw)):
-    x1 = pts1_raw[i][0]
-    y1 = pts1_raw[i][0]
-    x2 = pts2_raw[i][0]
-    y2 = pts2_raw[i][0]
-
-    u1 = Point(x1, y1)
-    u2 = Point(x2, y2)
-
-    X = LinearTriangulation(P1, u1, P2, u2)
-    points3d.append(X[1])
-
-plot3D(points3d)
+run()
