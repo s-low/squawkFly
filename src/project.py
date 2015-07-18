76536e05f52f61b2243cddeaa83e773b98b4066b
s-low <SAMUEL.P.LOW@GMAIL.COM>
2015-07-18 13:21:28 +0100

    re-implemented the cv2.projectPoints and got precisely the same data

--- src/project.py
+++ src/project.py
@@ -1,5 +1,6 @@
 #!/usr/local/bin/python
 
+import sys
 import cv2
 import cv2.cv as cv
 import numpy as np
@@ -8,10 +9,12 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import structureTools as struc
 
+np.set_printoptions(suppress=True)
 plt.style.use('ggplot')
 
 
 def main():
+    # cube
     data_3d = [[0, 0, 0],
                [0, 30, 0],
                [0, 0, 30],
@@ -25,35 +28,87 @@ def main():
 
     # artificial camera with f=5 units, cx = cy = 5 units
     K = struc.CalibArray(5, 5, 5)
-    distCoeffs = (0, 0, 0, 0)
+    dist = (0, 0, 0, 0)
 
     rt2 = math.sqrt(2)
 
     # 45 cc about z
-    rotation = np.mat([[1 / rt2, -1 / rt2, 0],
-                       [1 / rt2, 1 / rt2, 0],
-                       [0, 0, 1]])
+    z45cc = np.mat([[1 / rt2, -1 / rt2, 0],
+                    [1 / rt2, 1 / rt2, 0],
+                    [0, 0, 1]], dtype='float32')
 
-    dst, jacobian = cv2.Rodrigues(src=rotation)
+    z90cc = np.mat([[0, -1, 0], [1, 0, 0], [0, 0, 1]], dtype='float32')
+    x90cc = np.mat([[1, 0, 0], [0, 0, -1], [0, 1, 0]], dtype='float32')
+    y90cc = np.mat([[0, 0, 1], [0, 1, 0], [-1, 0, 0]], dtype='float32')
+
+    z90cc_vec, jacobian = cv2.Rodrigues(z90cc)
+    z90cc_vec, jacobian = cv2.Rodrigues(z90cc)
+    y90cc_vec, jacobian = cv2.Rodrigues(y90cc)
+    x90cc_vec, jacobian = cv2.Rodrigues(x90cc)
 
     # projections into image planes with the camera in different poses
     tvec = (120, 0, 120)
-    rvec = dst
-    img_pts1, jacobian = cv2.projectPoints(
-        data_3d, rvec, tvec, K, distCoeffs)
 
-    tvec = (-120, 0, 120)
-    rvec = -dst
-    img_pts2, jacobian = cv2.projectPoints(
-        data_3d, rvec, tvec, K, distCoeffs)
+    img_pts1 = project(data_3d, K, z90cc, tvec)
+
+    img_pts1_, jacobian = cv2.projectPoints(data_3d, z90cc_vec, tvec, K, dist)
+    # img_pts1_ = fixExtraneousParentheses(img_pts1)
 
     plotSimulation(data_3d)
 
-    print img_pts1
+    print "manual:\n", img_pts1
+    print "opencv:\n", img_pts1_
     plotImagePoints(img_pts1)
 
-    print img_pts2
-    plotImagePoints(img_pts2)
+    # print img_pts2
+    # plotImagePoints(img_pts2)
+
+
+# own implementation of cv2.projectPoints() - project 3d into 2d plane
+# given intrinsic, rotation matrix and translation vector
+def project(objectPoints, K, R, t):
+
+    print "--------- MANUAL PROJECTION -----------"
+    objectPoints = cv2.convertPointsToHomogeneous(objectPoints)
+    objectPoints = fixExtraneousParentheses(objectPoints)
+
+    imagePoints = []
+    print "K:\n", K
+
+    t = np.mat(t)
+    t = t.T
+    print "R:\n", R
+    print "t:\n", t
+
+    Rt = np.concatenate((R, t), 1)
+    print "R|t:\n", Rt
+
+    P = K * Rt
+    print "P = k(R|t):\n", P
+
+    for x in objectPoints:
+        x = np.mat(x).T
+        x_ = P * x
+        imagePoints.append(x_)
+
+    normed = []
+    for p in imagePoints:
+        p = p / p[2]
+        normed.append(p)
+
+    normed = np.array(normed)
+    normed = np.delete(normed, 2, 1)
+    return normed
+
+
+def fixExtraneousParentheses(points):
+    temp = []
+    for p in points:
+        p = p[0]
+        temp.append(p)
+
+    new = np.mat(temp)
+    return new
 
 
 # Plotting of the system in 3d
@@ -80,8 +135,8 @@ def plotSimulation(objectPoints):
 
 def plotImagePoints(imagePoints):
 
-    all_x = [point[0][0] for point in imagePoints]
-    all_y = [point[0][1] for point in imagePoints]
+    all_x = [point[0] for point in imagePoints]
+    all_y = [point[1] for point in imagePoints]
 
     plt.scatter(all_x, all_y)
     plt.scatter(all_x[0], all_y[0], color='r')
