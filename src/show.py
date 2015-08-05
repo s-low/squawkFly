da26082d75e5f0bc2ef1a15dd6995f27132d0ec3
s-low <SAMUEL.P.LOW@GMAIL.COM>
2015-08-05 15:26:12 +0100

    create little script to plot any x-space-y-\n file

---
+++ src/show.py
@@ -0,0 +1,28 @@
+#!/usr/local/bin/python
+
+import sys
+import matplotlib.pyplot as plt
+import plotting as plot
+
+plt.style.use('ggplot')
+
+
+filename = sys.argv[1]
+
+with open(filename) as datafile:
+    data = datafile.read()
+    datafile.close()
+
+data = data.split('\n')
+
+x = [row.split()[0] for row in data]
+y = [row.split()[1] for row in data]
+
+fig = plt.figure('Show.py')
+ax = plt.axes()
+# ax.set_title("Simple 2D Plot", y=1.03)
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+ax.plot(x, y, 'r.')
+
+plt.show()
