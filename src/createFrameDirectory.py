ffae11dfc1633c60479f6fd23fe5ede51f14ee09
s-low <SAMUEL.P.LOW@GMAIL.COM>
2015-08-06 20:58:16 +0100

    createFrameDirectory reads a video file and writes each frame to a new directory in same location as original

---
+++ src/createFrameDirectory.py
@@ -0,0 +1,38 @@
+#!/usr/local/bin/python
+
+import cv2
+import cv2.cv as cv
+import sys
+import os.path
+
+filename = sys.argv[1]
+
+cap = cv2.VideoCapture(filename)
+
+name, ext = os.path.splitext(filename)
+
+outdir = name
+
+if not os.path.exists(outdir):
+    os.makedirs(outdir)
+
+print "outdirectory:", outdir
+
+count = 0
+
+while (cap.isOpened()):
+
+    ret, frame = cap.read()
+
+    if ret is True:
+        count += 1
+        cv2.imshow('Current frame', frame)
+
+        outfilename = outdir + '/' + str(count) + '.png'
+        print "write:", outfilename
+        cv2.imwrite(outfilename, frame)
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
