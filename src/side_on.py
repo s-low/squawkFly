adc3ea0735345faef34ec2e7d4b106fbd87b4d50
s-low <SAMUEL.P.LOW@GMAIL.COM>
2015-08-23 15:29:50 +0100

    different representation of the goal in side_on

--- src/side_on.py
+++ src/side_on.py
@@ -9,6 +9,12 @@ import sys
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
+font = {'family': 'normal',
+        'weight': 'bold',
+        'size': 16}
+
+plt.rc('font', **font)
+
 filename = sys.argv[1]
 
 with open(filename) as datafile:
@@ -26,26 +32,33 @@ x = [row.split()[0] for row in data]
 y = [row.split()[1] for row in data]
 z = [row.split()[2] for row in data]
 
-bly = y[0]
-blz = z[0]
-tly = y[1]
-tlz = z[1]
-try_ = y[2]
-trz = z[2]
-bry = y[3]
-brz = z[3]
+bly = y.pop(0)
+blz = z.pop(0)
 
-fig = plt.figure('Top Down Projection')
+tly = y.pop(0)
+tlz = z.pop(0)
+
+try_ = y.pop(0)
+trz = z.pop(0)
+
+bry = y.pop(0)
+brz = z.pop(0)
+
+avgz = (float(blz) + float(tlz) + float(trz) + float(brz)) / 4
+
+fig = plt.figure('Side On Projection with Virtual Wall')
 ax = fig.add_subplot(111, aspect='equal')
 
-ax.set_xlabel("Z / m")
-ax.set_ylabel("Y / m")
+ax.set_xlabel("Distance Travelled to Goal / m")
+ax.set_ylabel("Height / m")
 ax.plot(z, y, 'k.')
-ax.plot([tlz, trz], [tly, try_], c='k')
-ax.plot([blz, tlz], [bly, tly], c='k')
-ax.plot([brz, trz], [bry, try_], c='k')
+# ax.plot([tlz, trz], [tly, try_], c='k')
+# ax.plot([blz, tlz], [bly, tly], c='k')
+# ax.plot([brz, trz], [bry, try_], c='k')
 
 # 2.14m wall at 9.14m
 ax.plot([9.14, 9.14], [0, 1.82], c='r', linewidth=2)
+ax.plot([avgz, avgz], [0, 2.44], c='k', linewidth=2)
+
 
 plt.show()
